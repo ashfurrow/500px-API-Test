33ef5704655fe69997f9f90fb3e0caba4986ed54
Ash Furrow <ash.furrow@gmail.com>
2011-08-21 17:16:19 -0400

    Adding APIHelper and APIConstants file.
    Change "University of New Brunswick" organization name in code comments.

---
+++ 500px API Test/500px API Test/APIHelper.h
@@ -0,0 +1,15 @@
+//
+//  APIHelper.h
+//  500px API Test
+//
+//  Created by Ash Furrow on 11-08-21.
+//  Copyright 2011 Ash Furrow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "APIConstants.h"
+
+@interface APIHelper : NSObject
+
+@end
---
+++ 500px API Test/500px API Test/APIHelper.m
@@ -0,0 +1,23 @@
+//
+//  APIHelper.m
+//  500px API Test
+//
+//  Created by Ash Furrow on 11-08-21.
+//  Copyright 2011 Ash Furrow. All rights reserved.
+//
+
+#import "APIHelper.h"
+
+@implementation APIHelper
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
