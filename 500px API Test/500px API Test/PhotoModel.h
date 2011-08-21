aa739127793f58c52875c5d9bbe4d3f08b5341df
Ash Furrow <ash.furrow@gmail.com>
2011-08-21 19:38:40 -0400

    Added a photo model class to abstract parsing the JSON data from the view controllers.

---
+++ 500px API Test/500px API Test/PhotoModel.h
@@ -0,0 +1,30 @@
+//
+//  PhotoModel.h
+//  500px API Test
+//
+//  Created by Ash Furrow on 11-08-21.
+//  Copyright 2011 University of New Brunswick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#define kPhotoModelNameKey          @"name"
+#define kPhotoModelCreatedDateKey   @"created_at"
+#define kPhotoModelImageURLKey      @"image_url"
+#define kPhotoModelRatingKey        @"rating"
+#define kPhotoModelIdentiferKey     @"id"
+#define kPhotoModelCategoryKey      @"category"
+
+@interface PhotoModel : NSObject
+
+-(id) initWithFetchedDictionary:(NSDictionary *)fetchedDictionary;
+
+@property (nonatomic, retain) NSString *name;
+@property (nonatomic, retain) NSString *createdDate;
+@property (nonatomic, retain) NSString *imageURL;
+@property (nonatomic, retain) NSString *rating;
+
+@property (nonatomic, assign) int identifier;
+@property (nonatomic, assign) int category;
+
+@end
