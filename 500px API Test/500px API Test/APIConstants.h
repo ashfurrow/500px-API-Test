5b6df4a5135035d2d435d7d7432f6d546eb6dc22
Ash Furrow <ash.furrow@gmail.com>
2011-08-21 17:16:19 -0400

    Adding APIHelper and APIConstants file.
    Change "University of New Brunswick" organization name in code comments.

---
+++ 500px API Test/500px API Test/APIConstants.h
@@ -0,0 +1,10 @@
+//
+//  APIConstants.h
+//  500px API Test
+//
+//  Created by Ash Furrow on 11-08-21.
+//  Copyright 2011 Ash Furrow. All rights reserved.
+//
+
+#define kAPIConsumerKey     @"Insert your consumer key here"
+#define kAPIConsumerSecret  @"Insert your consumer secrety here"
--- 500px API Test/500px API Test/RootViewController.m
+++ 500px API Test/500px API Test/RootViewController.m
@@ -3,7 +3,7 @@
 //  500px API Test
 //
 //  Created by Ash Furrow on 11-08-21.
-//  Copyright 2011 University of New Brunswick. All rights reserved.
+//  Copyright 2011 Ash Furrow. All rights reserved.
 //
 
 #import "RootViewController.h"
