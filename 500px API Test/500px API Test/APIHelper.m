845449b4a87ca3eff96fb88f3ef5c0b64c966459
Ash Furrow <ash.furrow@gmail.com>
2011-08-21 18:13:53 -0400

    Moved fetching logic into background queue.

--- 500px API Test/500px API Test/APIHelper.m
+++ 500px API Test/500px API Test/APIHelper.m
@@ -14,13 +14,22 @@
 
 @interface APIHelper (Private)
 
-+(NSString *)signURL:(NSString *)unsignedURL;
++(NSString *) signURL:(NSString *)unsignedURL;
++(dispatch_queue_t) backgroundQueue;
 
 @end
 
 @implementation APIHelper
 
 #pragma mark - Private Methods
+
++(dispatch_queue_t) backgroundQueue
+{
+    static dispatch_queue_t queue = NULL;
+    if (queue == NULL)
+        queue = dispatch_queue_create("com.500px.backgroundqueue", NULL);
+    return queue;
+}
 
 +(NSString *)signURL:(NSString *)unsignedURL
 {
@@ -31,32 +40,42 @@
 
 #pragma mark - Public Methods
 
-+ (NSArray *) fetchSomeData
++ (void) fetchPopularPhotosWithCallback:(CallbackBlock)callbackBlock andErrorBlock:(ErrorBlock)errorblock
 {
-    NSURL *url = [NSURL URLWithString:[APIHelper signURL:[NSString stringWithFormat:@"%@photos?feature=editors", kAPIURLStub]]];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    [request setHTTPMethod:@"GET"];
-    
-    NSHTTPURLResponse *response;
-    NSError *error = nil;
-    NSData *fetchedData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-    
-    if (error)
-    {
-        NSLog(@"Something went wrong fetching from API: %@", error);
-        return nil;
-    }
-    if ([response statusCode] != 200)
-    {
-        NSLog(@"Fetching from API returned non-200 response code: %d", [response statusCode]);
-    }
-    
-    NSString *fetchedDataString = [[[NSString alloc] initWithData:fetchedData encoding:NSUTF8StringEncoding] autorelease];
-    NSArray *fetchedArray = [fetchedDataString JSONValue];
-    
-    NSLog(@"Received the following response:%@", fetchedArray);
-    
-    return fetchedArray;
+    dispatch_async([APIHelper backgroundQueue], ^{
+        NSURL *url = [NSURL URLWithString:[APIHelper signURL:[NSString stringWithFormat:@"%@photos?feature=editors", kAPIURLStub]]];
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+        [request setHTTPMethod:@"GET"];
+        
+        NSHTTPURLResponse *response;
+        NSError *error = nil;
+        NSData *fetchedData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
+        
+        if (error)
+        {
+            NSLog(@"Something went wrong fetching from API: %@", error);
+            if (errorblock != NULL)
+            {
+                dispatch_async(dispatch_get_main_queue(), ^(void) {
+                    errorblock(error);
+                });
+            }
+        }
+        if ([response statusCode] != 200)
+        {
+            NSLog(@"Fetching from API returned non-200 response code: %d", [response statusCode]);
+        }
+        
+        NSString *fetchedDataString = [[[NSString alloc] initWithData:fetchedData encoding:NSUTF8StringEncoding] autorelease];
+        NSArray *fetchedArray = [[fetchedDataString JSONValue] valueForKey:@"photos"];
+        
+        NSLog(@"Received the following response:%@", fetchedArray);
+        
+        dispatch_async(dispatch_get_main_queue(), ^(void) {
+            callbackBlock(fetchedArray);
+        });
+
+    });
 }
 
 @end
