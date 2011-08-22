f5306a0dda80edf2fe8edd1c88561988fc65e8a9
Ash Furrow <ash.furrow@gmail.com>
2011-08-21 21:11:00 -0400

    Re-adding APIConstants.h

---
+++ 500px API Test/500px API Test/APIConstants.h
@@ -0,0 +1,14 @@
+//
+//  APIConstants.h
+//  500px API Test
+//
+//  Created by Ash Furrow on 11-08-21.
+//  Copyright 2011 Ash Furrow. All rights reserved.
+//
+
+#define kAPIConsumerKey     @"Insert your consumer key here"
+#define kAPIConsumerSecret  @"Insert your consumer secrety here" 
+
+#define kRequestTokenURL    @"https://api.500px.com/v1/oauth/request_token"
+#define kAccessTokenURL     @"https://api.500px.com/v1/oauth/access_token"
+#define kAuthorizeURL       @"https://api.500px.com/v1/oauth/authorize"
